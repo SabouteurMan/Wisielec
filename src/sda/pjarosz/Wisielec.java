a39f7f3d4512274645bafbdad21bed39bd447af2
pjaro <pjarosz27@wp.pl>
2020-05-17 12:15:46 +0200

    Pierwsza wersja wisielca bez rysowania

--- src/sda/pjarosz/Wisielec.java
+++ src/sda/pjarosz/Wisielec.java
@@ -27,23 +27,44 @@ public class Wisielec {
         System.out.println("*******Gra w wisielca********");
 
         char c;
+        int foults = 10;
         do {
             System.out.println("zgadujesz kolejne litery w słowie:");
             System.out.println(guessedWord(word, guessed));
             System.out.println();
             System.out.print("Podaj litere: ");
             c = sc1.next().charAt(0);
+            if(!tryLetter(word, guessed, c)){
+                foults--;
+                //rysowanie wisielca
+                for (int i = 0; i < 10 - foults; i++) {
+                    System.out.printf("X ");
+                }
+                System.out.println();
+            }
 
-
-        }while(true);
-
+        } while (foults > 0 && !isGuessed(guessed));
+        if(foults == 0){
+            System.out.println("\n************");
+            System.out.println("*WISISZ*");
+            System.out.println("************\n");
+        }
+else System.out.println("\nBrawo, odgadłes słowo: " + word);
 
     }//end main
 
-    private static boolean tryLetter(String word, boolean[] guess, char c){
+    private static boolean isGuessed(boolean[] guess){
+        for (int i = 0; i < guess.length; i++) {
+            if(guess[i] == false)return false;
+
+        }
+        return true;
+    }
+
+    private static boolean tryLetter(String word, boolean[] guess, char c) {
         boolean goal = false;
         for (int i = 0; i < word.length(); i++) {
-            if(word.charAt(i) == c){
+            if (word.charAt(i) == c) {
                 guess[i] = true;
                 goal = true;
             }
@@ -52,12 +73,12 @@ public class Wisielec {
         return goal;
     }
 
-    private static String guessedWord(String word, boolean[] guess){
+    private static String guessedWord(String word, boolean[] guess) {
 
         StringBuilder s = new StringBuilder(word);
         for (int i = 0; i < guess.length; i++) {
-            if(guess[i] == false){
-                s.replace(i, i+1, ".");
+            if (guess[i] == false) {
+                s.replace(i, i + 1, ".");
             }
         }
         return s.toString();
